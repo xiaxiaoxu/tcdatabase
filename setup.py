f8f9357cd6e7048e3e257b9fc0d18b844c1f4055
xiaxiaoxu <xiaxiaoxu1987@163.com>
2020-09-22 20:42:39 +0800

    关联本地文件夹

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tcdatabase',
+    version='0.1',
+    packages=find_packages(),
+    author = 'xiaxx',
+    author_email='xiaxx@jiguang.cn',
+    url='None',
+)
