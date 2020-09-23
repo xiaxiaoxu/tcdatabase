4e4e0a036182e9440ce52ea3cd742b458bb20c7d
xiaxiaoxu <xiaxiaoxu1987@163.com>
2020-09-23 10:24:56 +0800

    add file

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='tcdatabase',
     version='0.1',
     packages=find_packages(),
+    package_data = {
+        'tcdatabase':['.py','']
+    },
     author = 'xiaxx',
     author_email='xiaxx@jiguang.cn',
     url='None',
