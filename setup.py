744b1231172bca742d0f06b3000bb14e4d52e6d2
xiaxiaoxu <xiaxiaoxu1987@163.com>
2020-09-23 15:54:01 +0800

    add file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='tctools',
     version='0.1',
-    packages=find_packages('tctools'),
+    packages=find_packages(),
     package_data={
         'tctools':["*.py"]
     },
